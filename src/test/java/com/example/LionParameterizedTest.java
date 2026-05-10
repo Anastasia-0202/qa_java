020316c0937213a1e759c446ab8262fefbd21984
Anastasia <anastasia0202@inbox.ru>
2026-05-10 16:04:30 +0300

    Initial commit: добавлен проект Sprint_6

---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LionParameterizedTest {
+
+    static Stream<Arguments> setSexForLion() {
+        return Stream.of(
+                Arguments.of("Самец", true),
+                Arguments.of("Самка", false)
+        );
+    }
+
+    @ParameterizedTest(name = "Класс Lion. Пол: {0}")
+    @MethodSource("setSexForLion")
+    @DisplayName("Проверка наличия гривы у льва")
+    public void doesHaveMane_IsCorrect(String sex, boolean expectedHasMane) throws Exception {
+        Lion lion = new Lion(sex);
+
+
+        assertEquals(expectedHasMane, lion.doesHaveMane());
+    }
+}
---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LionTest {
+
+    @Test
+    public void createLion_ExpectedException() {
+        assertThrows(Exception.class, () -> new Lion("Не определено"));
+    }
+
+    @Test
+    public void getKittens_IsCorrect() throws Exception {
+        Lion lion = new Lion("Самец");
+        int expectedCount = 5;
+
+        assertEquals(
+                expectedCount,
+                lion.getKittens()
+        );
+    }
+
+    @Test
+    public void getFood_IsCorrect() throws Exception {
+        Lion lion = new Lion("Самка");
+        List<String> expectedListOfFood = List.of("Пища");
+
+        assertEquals(
+                expectedListOfFood,
+                lion.getFood()
+        );
+    }
+}
